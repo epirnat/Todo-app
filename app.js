4f5491b09f8eb6ace1ac44f62d902beab1418b02
epirnat <edi.pirnat@gmail.com>
2020-08-05 07:38:28 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const todos = [{
+    text: 'Return videotapes',
+    completed: false
+  }, {
+    text: 'Do laundry',
+    completed: true
+  }, {
+    text: 'Study for exams',
+    completed: false
+  }, {
+    text: 'Lift weights',
+    completed: false
+  }, {
+    text: 'Buy oats',
+    completed: true
+  }];
