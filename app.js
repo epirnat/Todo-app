385ba0906357398c8d5bda7b87f0a6794cc295bc
epirnat <edi.pirnat@gmail.com>
2020-08-09 12:12:16 +0200

    todo filter now works

--- app.js
+++ app.js
@@ -15,24 +15,38 @@ const todos = [{
     completed: true
   }];
 
-  const incompleteTodos = todos.filter(function (todo) {
+  const renderTodos = function (todos, filters) {
+    const filteredTodos = todos.filter(function (todo) {
+      return todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
+    })
+    const incompleteTodos = filteredTodos.filter (function (todo) {
       return !todo.completed;
+    })
+    
+    document.querySelector('#todos').innerHTML = ''
+  
+    const summary = document.createElement('h2');
+    summary.textContent = `You have ${incompleteTodos.length} todos left`;
+    document.querySelector('#todos').appendChild(summary);
+    
+    filteredTodos.forEach(function (todo) {
+      const p = document.createElement('p');
+      p.textContent = todo.text;
+      document.querySelector('#todos').appendChild(p);
+    })
+  }
+  
+  renderTodos(todos, filters)
+  
+  document.querySelector('#add-todo').addEventListener('click', function (e) {
+    console.log('I\'m adding a new todo');
   })
-
-const summary = document.createElement('h2');
-summary.textContent = `You have ${incompleteTodos.length} left`;
-document.querySelector('body').appendChild(summary);
-
-todos.forEach(function (todo) {
-    const p = document.createElement('p');
-    p.textContent = todo.text;
-    document.querySelector('body').appendChild(p);
-})
-
-document.querySelector('#add-todo').addEventListener('click', function (e) {
-  console.log('Adding new todo..');
-})
-
-document.querySelector('#new-todo-text').addEventListener('input', function (e) {
-  console.log(e.target.value);
-})
+  
+  document.querySelector('#new-todo-text').addEventListener('input', function (e) {
+    console.log(e.target.value);
+  })
+  
+  document.querySelector('#search-text').addEventListener('input', function (e) {
+    filters.searchText = e.target.value
+    renderTodos(todos, filters)
+  })
