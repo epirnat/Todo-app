5a20368de63f874aac06eb71babcf1cf98e6afd8
epirnat <edi.pirnat@gmail.com>
2020-08-09 12:23:15 +0200

    filter todos functionality added

--- app.js
+++ app.js
@@ -15,38 +15,42 @@ const todos = [{
     completed: true
   }];
 
-  const renderTodos = function (todos, filters) {
-    const filteredTodos = todos.filter(function (todo) {
-      return todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
-    })
-    const incompleteTodos = filteredTodos.filter (function (todo) {
-      return !todo.completed;
-    })
-    
-    document.querySelector('#todos').innerHTML = ''
+const filters = {
+  searchText: ''
+}
+
+const renderTodos = function (todos, filters) {
+  const filteredTodos = todos.filter(function (todo) {
+    return todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
+  })
+  const incompleteTodos = filteredTodos.filter (function (todo) {
+    return !todo.completed;
+  })
   
-    const summary = document.createElement('h2');
-    summary.textContent = `You have ${incompleteTodos.length} todos left`;
-    document.querySelector('#todos').appendChild(summary);
-    
-    filteredTodos.forEach(function (todo) {
-      const p = document.createElement('p');
-      p.textContent = todo.text;
-      document.querySelector('#todos').appendChild(p);
-    })
-  }
+  document.querySelector('#todos').innerHTML = ''
+
+  const summary = document.createElement('h2');
+  summary.textContent = `You have ${incompleteTodos.length} todos left`;
+  document.querySelector('#todos').appendChild(summary);
   
+  filteredTodos.forEach(function (todo) {
+    const p = document.createElement('p');
+    p.textContent = todo.text;
+    document.querySelector('#todos').appendChild(p);
+  })
+}
+
+renderTodos(todos, filters)
+
+document.querySelector('#add-todo').addEventListener('click', function (e) {
+  console.log('I\'m adding a new todo');
+})
+
+document.querySelector('#new-todo-text').addEventListener('input', function (e) {
+  console.log(e.target.value);
+})
+
+document.querySelector('#search-text').addEventListener('input', function (e) {
+  filters.searchText = e.target.value
   renderTodos(todos, filters)
-  
-  document.querySelector('#add-todo').addEventListener('click', function (e) {
-    console.log('I\'m adding a new todo');
-  })
-  
-  document.querySelector('#new-todo-text').addEventListener('input', function (e) {
-    console.log(e.target.value);
-  })
-  
-  document.querySelector('#search-text').addEventListener('input', function (e) {
-    filters.searchText = e.target.value
-    renderTodos(todos, filters)
-  })
+})
